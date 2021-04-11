f97cfb2951b433795340254491634120ca0d792b
Matthieu Berthomé <matthieu@mmea.fr>
2021-04-11 15:03:22 +0200

    updates

--- pelicanconf.py
+++ pelicanconf.py
@@ -51,10 +51,40 @@ DEBUG = False
 PLUGIN_PATHS = [os.path.expanduser('~/pelican-plugins')]
 PLUGINS = ['i18n_subsites']
 
+ICONS = {
+            "graphql": open('themes/resume/static/images/graphql.svg').read().replace('<svg', '<svg width=45'),
+            "openapi": open('themes/resume/static/images/openapi.svg').read().replace('<svg', '<svg width=45'),
+            "open-source": open('themes/resume/static/images/open-source.svg').read().replace('<svg', '<svg width=45')
+        }
+
 
 def get_data(lang):
-    with open(os.path.join('data', lang, 'resume.json'), 'r') as resume:
-        return json.load(resume)
+    with open(os.path.join('data', lang, 'resume.json'), 'r') as resume_file:
+        resume = json.load(resume_file)
+        if 'email' in resume['basics']:
+            resume['basics']['email'] = obfuscate_string(resume['basics']['email'])
+        if resume['basics']['x-emails']:
+            resume['basics']['x-emails'] = [obfuscate_string(e) for e  in resume['basics']['x-emails']]
+        resume['basics']['phone'] = obfuscate_string(resume['basics']['phone'])
+
+        for key in 'work', 'projects':
+            kc = f"{key}_by_category"
+            resume[kc] = {}
+            for item in resume.get(key, []):
+                resume[kc].setdefault(item['x-category'], []).append(item)
+            for item in resume.get(key, []):
+                if 'x-stack' in item:
+                    def get_wordmark(w):
+                        if w == 'd3js':
+                            return False
+                        return True
+                    item['x-stack'] = [
+                        {
+                            "icon": ICONS.get(stack, f"<i class =\"devicon-{stack}-plain{'-wordmark' if get_wordmark(stack) else ''}\"></i>")
+                        } for stack in
+                        item['x-stack']
+                    ]
+        return resume
 
 
 I18N_SUBSITES = {
@@ -76,18 +106,9 @@ JINJA_ENVIRONMENT = {
 I18N_GETTEXT_NEWSTYLE = True
 
 RESUME = get_data('en')
-if 'email' in RESUME['basics']:
-    RESUME['basics']['email'] = obfuscate_string(RESUME['basics']['email'])
-if RESUME['basics']['x-emails']:
-    RESUME['basics']['x-emails'] = [obfuscate_string(e) for e in RESUME['basics']['x-emails']]
-RESUME['basics']['phone'] = obfuscate_string(RESUME['basics']['phone'])
-
-RESUME['work_by_category'] = {}
-for work_item in RESUME.get('work', []):
-    RESUME['work_by_category'].setdefault(work_item['x-category'], []).append(work_item)
 
 
-PROJECT_INTRO = "<span lang=\"en\">Open-source contributions</span><span lang="fr">Contributions open-source</span>"
+PROJECT_INTRO = "<span lang=\"en\">Open-source contributions</span><span lang=\"fr\">Contributions open-source</span>"
 
 ######
 #
